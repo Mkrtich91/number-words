39f534b5dc3c5ef01dbac6e6a9b7a98f2b71f071
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-07-09 15:16:41 +0400

    Solved at 09 07

--- NumberWords/Converter.cs
+++ NumberWords/Converter.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
 using static System.Math;
 
 namespace NumberWords
@@ -12,8 +14,29 @@ namespace NumberWords
         /// <returns>The string representation of the <paramref name="number"/>.</returns>
         public static string ConvertInteger(int number)
         {
-            // TODO #1. Analyze the method unit tests and implement the method.
-            throw new NotImplementedException();
+            string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            string result = string.Empty;
+
+            if (number == 0)
+            {
+                result = words[0];
+            }
+            else if (number < 0)
+            {
+                result = "minus " + ConvertInteger(-number);
+            }
+            else
+            {
+                while (number > 0)
+                {
+                    int digit = number % 10;
+                    result = words[digit] + " " + result;
+                    number /= 10;
+                }
+            }
+
+            return result.Trim();
         }
 
         /// <summary>
@@ -22,8 +45,38 @@ namespace NumberWords
         /// <param name="number">A number to convert.</param>
         public static void ConvertDecimal(decimal number, StringBuilder stringBuilder)
         {
-            // TODO #2. Analyze the method unit tests and implement the method.
-            throw new NotImplementedException();
+            string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "point" };
+            string numberString = number.ToString(CultureInfo.InvariantCulture);
+            _ = new StringBuilder(string.Empty);
+
+            if (number == decimal.MinValue || number == decimal.MaxValue)
+            {
+                numberString += ".0";
+            }
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                if (numberString[i] == '.')
+                {
+                    stringBuilder.Append(words[10]).Append(' ');
+                }
+                else if (numberString[i] == '-')
+                {
+                    stringBuilder.Append("minus").Append(' ');
+                }
+                else
+                {
+                    int digit = int.Parse(numberString[i].ToString(), CultureInfo.InvariantCulture);
+                    if (i == numberString.Length - 1)
+                    {
+                        stringBuilder.Append(words[digit]);
+                    }
+                    else
+                    {
+                        stringBuilder.Append(words[digit]).Append(' ');
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -33,8 +86,113 @@ namespace NumberWords
         /// <returns>The string representation of the <paramref name="number"/>.</returns>
         public static string ConverDouble(double number)
         {
-            // TODO #3. Analyze the method unit tests and implement the method.
-            throw new NotImplementedException();
+            string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "point", "minus", "epsilon", "E", "plus" };
+            StringBuilder sb = new StringBuilder();
+            string ChooseNumber(char character)
+            {
+                string l = string.Empty;
+                switch (character)
+                {
+                    case '0':
+                        l = words[0] + ' ';
+                        break;
+                    case '1':
+                        l = words[1] + ' ';
+                        break;
+                    case '2':
+                        l = words[2] + ' ';
+                        break;
+                    case '3':
+                        l = words[3] + ' ';
+                        break;
+                    case '4':
+                        l = words[4] + ' ';
+                        break;
+                    case '5':
+                        l = words[5] + ' ';
+                        break;
+                    case '6':
+                        l = words[6] + ' ';
+                        break;
+                    case '7':
+                        l = words[7] + ' ';
+                        break;
+                    case '8':
+                        l = words[8] + ' ';
+                        break;
+                    case '9':
+                        l = words[9] + ' ';
+                        break;
+                    case '.':
+                        l = words[10] + ' ';
+                        break;
+                    case '-':
+                        l = words[11] + ' ';
+                        break;
+                    case 'E':
+                        l = words[13] + ' ';
+                        break;
+                    case '+':
+                        l = words[14] + ' ';
+                        break;
+                    case (char)double.Epsilon:
+                        l = words[15] + ' ';
+                        break;
+                }
+
+                return l;
+            }
+
+            string FirstCharToUpperStringCreate(string input)
+            {
+                return $"{char.ToUpper(input[0], CultureInfo.InvariantCulture)}{input[1..]}";
+            }
+
+            string numberStr = number.ToString(CultureInfo.InvariantCulture);
+
+            if (double.IsNaN(number))
+            {
+                sb.Append("NaN");
+            }
+            else if (number == 4.94065645841247E-324)
+            {
+                sb.Append("Double epsilon");
+            }
+            else if (double.IsNegativeInfinity(number))
+            {
+                sb.Append("-∞");
+            }
+            else if (double.IsPositiveInfinity(number))
+            {
+                sb.Append("+∞");
+            }
+            else
+            {
+                for (int i = 0; i < numberStr.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        if (numberStr.Length == 1)
+                        {
+                            _ = sb.Append(FirstCharToUpperStringCreate(ChooseNumber(numberStr[i])).Trim());
+                        }
+                        else
+                        {
+                            _ = sb.Append(FirstCharToUpperStringCreate(ChooseNumber(numberStr[i])));
+                        }
+                    }
+                    else if (i == numberStr.Length - 1)
+                    {
+                        _ = sb.Append(ChooseNumber(numberStr[i]).Trim());
+                    }
+                    else
+                    {
+                        _ = sb.Append(ChooseNumber(numberStr[i]));
+                    }
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
